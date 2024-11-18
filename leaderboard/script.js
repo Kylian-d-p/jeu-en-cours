3a6e92850b69938d6a823ab4329af1ec065324d4
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-11-18 17:06:03 +0100

    remove random player data generation from leaderboard script

--- leaderboard/script.js
+++ leaderboard/script.js
@@ -18,22 +18,3 @@ const addPlayer = (position, name, time) => {
 
   playersContainer.appendChild(playerElement)
 }
-
-// Ajout de valeurs fictives aléatoires
-
-const pseudos = ["Jean", "Paul", "Jacques", "Marie", "Pierre", "Lucie", "Julie", "Julien", "Lucas", "Léa"]
-pseudos.sort(() => Math.random() - 0.5)
-
-let datas = []
-
-for (let i = 0; i < pseudos.length; i++) {
-  datas.push({
-    position: i+1,
-    name: pseudos[i],
-    time: i === 0 ? 30 + Math.floor(Math.random() * 30) : datas[i-1].time + Math.floor(Math.random() * 5) + 1
-  })
-}
-
-for (const data of datas) {
-  addPlayer(data.position, data.name, `${data.time}s`)
-}
